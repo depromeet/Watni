91cfb5b1a96208d205dd458d6b51abe76d545719
zion830 <zion830@honglab.org>
2019-12-28 16:43:31 +0900

    feat: ToastUtil 추가

---
+++ app/src/main/java/com/depromeet/watni/util/ToastUtil.kt
@@ -0,0 +1,13 @@
+package com.depromeet.watni.util
+
+import android.widget.Toast
+import com.depromeet.watni.MainApplication
+
+fun showToast(text: String) {
+    Toast.makeText(MainApplication.getContext(), text, Toast.LENGTH_SHORT).show()
+}
+
+fun showToast(resId: Int) {
+    val context = MainApplication.getContext()
+    Toast.makeText(context, context.getString(resId), Toast.LENGTH_SHORT).show()
+}

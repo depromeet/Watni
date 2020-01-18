07cdbd93cb4b8847d11d19d469ef69b42be3f079
zion830 <zion830@honglab.org>
2020-01-18 21:33:53 +0900

    feat: Base 클래스 MVVM에 맞춰서 변경

---
+++ app/src/main/java/com/depromeet/watni/base/BaseActivity.kt
@@ -0,0 +1,20 @@
+package com.depromeet.watni.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(
+    @LayoutRes val layoutId: Int
+) : AppCompatActivity() {
+    protected lateinit var binding: B
+        private set
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutId)
+    }
+}

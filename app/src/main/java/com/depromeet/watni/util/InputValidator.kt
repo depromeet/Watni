ea5919dcb5bf027f4978dc4a6f613ba2949849c5
zion830 <zion830@honglab.org>
2020-02-16 23:54:28 +0900

    feat: 유효성 검사 로직 추가

--- app/src/main/java/com/depromeet/watni/util/InputValidator.kt
+++ app/src/main/java/com/depromeet/watni/util/InputValidator.kt
@@ -8,10 +8,18 @@ object InputValidator {
     private const val PWD_LENGTH_MAX = 12
     private const val PWD_PATTERN = "^[A-Za-z0-9]{$PWD_LENGTH_MIN,$PWD_LENGTH_MAX}$"
 
-    fun isNameValid(name: String) = name.length in NAME_LENGTH_MIN..NAME_LENGTH_MAX
-            && name.isNotBlank()
+    fun isValidName(name: String?) =
+        !name.isNullOrBlank() && name.length in NAME_LENGTH_MIN..NAME_LENGTH_MAX
 
-    fun isEmailValid(email: String) = email.contains("@")
+    fun isValidEmail(email: String?) = !email.isNullOrBlank() && email.contains("@")
 
-    fun isPwdValid(pwd: String) = PWD_PATTERN.toRegex().matches(pwd)
+    fun isValidPwd(pwd: String?) = !pwd.isNullOrBlank()
+            && PWD_PATTERN.toRegex().matches(pwd)
+
+    fun isSamePwd(pwd: String?, pwdConfirm: String?) = !pwd.isNullOrBlank()
+            && !pwdConfirm.isNullOrBlank()
+            && (pwd == pwdConfirm)
+
+    fun isJoinInfoValid(name: String?, email: String?, pwd: String?, pwdConfirm: String?) =
+        isValidName(name) && isValidEmail(email) && isSamePwd(pwd, pwdConfirm)
 }

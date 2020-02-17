d88b6466d2f76885494126865845d2f5e9026dba
zion830 <zion830@honglab.org>
2020-02-18 00:13:19 +0900

    feat: 네트워크 응답 상수 정의

---
+++ app/src/main/java/com/depromeet/watni/network/NetworkConstant.kt
@@ -0,0 +1,14 @@
+package com.depromeet.watni.network
+
+import com.depromeet.watni.R
+import com.depromeet.watni.util.ResourceUtil
+
+/*
+ * Created by yunji on 2020-02-18
+ */
+val NETWORK_ERROR_MSG = ResourceUtil.getString(R.string.msg_network_err)
+val ALREADY_USED_EMAIL_MSG = ResourceUtil.getString(R.string.join_already_used_email)
+
+const val SUCCESS_CODE = 200
+const val FAIL_CODE = 400
+const val ERROR_CODE = 404

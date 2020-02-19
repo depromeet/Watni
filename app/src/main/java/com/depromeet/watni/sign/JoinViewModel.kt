f0ce15967717a680b606f26a997a6c447aea9613
yunji <yunji@webtoonscorp.com>
2020-02-19 11:22:00 +0900

    feat: Login View observing 처리

--- app/src/main/java/com/depromeet/watni/sign/JoinViewModel.kt
+++ app/src/main/java/com/depromeet/watni/sign/JoinViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.depromeet.watni.R
 import com.depromeet.watni.model.request.UserJoin
+import com.depromeet.watni.model.source.SignDataSource
 import com.depromeet.watni.model.source.SignRepository
 import com.depromeet.watni.util.InputValidator
 import com.depromeet.watni.util.ResourceUtil
@@ -13,10 +14,13 @@ import com.depromeet.watni.util.ResourceUtil
  * Created by yunji on 2020-01-28
  */
 class JoinViewModel : ViewModel() {
+    private val signRepository: SignDataSource = SignRepository()
+
     private val _joinStatus = MutableLiveData<Boolean>()
     private val _isLoading = MutableLiveData<Boolean>()
     private val _joinAvailable = MutableLiveData<Boolean>()
     private val _msgText = MutableLiveData<String>()
+
     val emailText = MutableLiveData<String>()
     val pwdText = MutableLiveData<String>()
     val pwdConfirmText = MutableLiveData<String>()
@@ -41,7 +45,7 @@ class JoinViewModel : ViewModel() {
             emailText.value!!, nameText.value!!, pwdText.value!!, pwdConfirmText.value!!
         )
 
-        SignRepository.userJoin(user, success = {
+        signRepository.userJoin(user, success = {
             _msgText.value = ResourceUtil.getString(R.string.join_success)
             _joinStatus.value = true
             _isLoading.value = false
--- app/src/main/java/com/depromeet/watni/sign/LoginViewModel.kt
+++ app/src/main/java/com/depromeet/watni/sign/LoginViewModel.kt
@@ -3,23 +3,29 @@ package com.depromeet.watni.sign
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.depromeet.watni.model.request.UserLogin
+import com.depromeet.watni.model.source.SignDataSource
+import com.depromeet.watni.model.source.SignRepository
 
 /**
  * Created by yunji on 2020-01-28
  */
 class LoginViewModel : ViewModel() {
+    private val signRepository: SignDataSource = SignRepository()
+
     private val _isLoading = MutableLiveData<Boolean>()
     private val _loginStatus = MutableLiveData<Boolean>()
     private val _loginAvailable = MutableLiveData<Boolean>()
     private val _msgText = MutableLiveData<String>()
+
     val emailText = MutableLiveData<String>()
     val pwdText = MutableLiveData<String>()
 
-    val loginStatus: LiveData<Boolean>
-        get() = _loginStatus
-
     val isLoading: LiveData<Boolean>
         get() = _isLoading
+
+    val loginStatus: LiveData<Boolean>
+        get() = _loginStatus
 
     val loginAvailable: LiveData<Boolean>
         get() = _loginAvailable
@@ -28,10 +34,17 @@ class LoginViewModel : ViewModel() {
         get() = _msgText
 
     fun attemptLogin() {
-
+        val user = UserLogin(emailText.value!!, pwdText.value!!)
+        signRepository.userLogin(user, success = {
+            _loginStatus.value = true
+            _isLoading.value = false
+        }, failed = { _, msg ->
+            _msgText.value = msg
+            _isLoading.value = false
+        })
     }
 
     fun updateLoginAvailable() {
-        _loginAvailable.value = !emailText.value.isNullOrBlank() && !pwdText.value.isNullOrBlank()
+        _loginAvailable.value = !(emailText.value.isNullOrBlank() || pwdText.value.isNullOrBlank())
     }
 }

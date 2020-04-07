1268a4c0cfd76d2d062d3512a5049f8658086d6c
zion830 <zion830@honglab.org>
2020-04-08 04:31:43 +0900

    feat: base class에 메서드 추가
    - Activity에 FragmentManager 조작 메서드 추가
    - Fragment에 viewModel을 abstract로 선언

--- app/src/main/java/com/depromeet/watni/base/BaseActivity.kt
+++ app/src/main/java/com/depromeet/watni/base/BaseActivity.kt
@@ -5,6 +5,9 @@ import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.depromeet.watni.util.addNewFragment
 
 abstract class BaseActivity<B : ViewDataBinding>(
     @LayoutRes val layoutId: Int
@@ -16,5 +19,17 @@ abstract class BaseActivity<B : ViewDataBinding>(
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, layoutId)
+    }
+
+    open fun startNewFragment(containerId: Int, fragment: Fragment, tag: String? = null) {
+        supportFragmentManager.addNewFragment(containerId, fragment, tag)
+    }
+
+    open fun removeCurrentFragment() {
+        supportFragmentManager.popBackStack()
+    }
+
+    open fun removeAllFragment() {
+        supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 }
--- app/src/main/java/com/depromeet/watni/base/BaseFragment.kt
+++ app/src/main/java/com/depromeet/watni/base/BaseFragment.kt
@@ -8,18 +8,24 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import com.depromeet.watni.BR
 
 /*
  * Created by yunji on 2020-02-22
  */
-abstract class BaseFragment<B : ViewDataBinding>(
+abstract class BaseFragment<B : ViewDataBinding, VM : ViewModel>(
     @LayoutRes private val layoutResId: Int
 ) : Fragment(layoutResId) {
     protected lateinit var binding: B
 
+    protected abstract val viewModel: VM
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
         binding.lifecycleOwner = this
+        binding.setVariable(BR.viewModel, viewModel)
+
         return binding.root
     }
 }

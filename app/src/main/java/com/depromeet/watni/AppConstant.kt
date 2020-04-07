d930312488ccd0e50c19963e8fb79c4419acbcca
zion830 <zion830@honglab.org>
2020-04-08 02:47:44 +0900

    refactor: xml indent 변경

--- app/src/main/java/com/depromeet/watni/AppConstant.kt
+++ app/src/main/java/com/depromeet/watni/AppConstant.kt
@@ -3,7 +3,3 @@ package com.depromeet.watni
 /*
  * Created by yunji on 2020-02-22
  */
-const val EXTRA_IS_ENTERING = "is_entering"
-const val START_GROUP_CREATE = 10
-const val START_GROUP_ENTER = 11
-const val START_GROUP_SUCCESS = 12

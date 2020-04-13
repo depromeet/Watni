0ca7aae08137139f1eefd82bb129e866515cb6a6
zion830 <zion830@honglab.org>
2020-04-13 19:40:00 +0900

    feat: 애니메이션 방향 수정

--- app/src/main/java/com/depromeet/watni/ext/FragmentExt.kt
+++ app/src/main/java/com/depromeet/watni/ext/FragmentExt.kt
@@ -9,7 +9,7 @@ import com.depromeet.watni.R
  */
 fun FragmentManager.addNewFragment(containerId: Int, fragment: Fragment, tag: String? = null) {
     beginTransaction()
-        .setCustomAnimations(R.anim.start_from_left, R.anim.exit_to_left, R.anim.start_from_left, R.anim.exit_to_right)
+        .setCustomAnimations(R.anim.start_from_right, R.anim.exit_to_left, R.anim.start_from_left, R.anim.exit_to_right)
         .add(containerId, fragment, tag)
         .addToBackStack(null)
         .commit()
@@ -17,7 +17,7 @@ fun FragmentManager.addNewFragment(containerId: Int, fragment: Fragment, tag: St
 
 fun FragmentManager.replaceFragment(containerId: Int, fragment: Fragment, tag: String? = null) {
     beginTransaction()
-        .setCustomAnimations(R.anim.start_from_left, R.anim.exit_to_left, R.anim.start_from_left, R.anim.exit_to_right)
+        .setCustomAnimations(R.anim.start_from_right, R.anim.exit_to_left, R.anim.start_from_left, R.anim.exit_to_right)
         .replace(containerId, fragment, tag)
         .addToBackStack(null)
         .commit()

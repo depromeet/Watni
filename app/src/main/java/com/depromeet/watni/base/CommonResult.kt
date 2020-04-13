5fb58dbddab11fbd6ef081c758918164155819f9
zion830 <zion830@honglab.org>
2020-04-13 22:10:43 +0900

    feat: CommonResult 추가

---
+++ app/src/main/java/com/depromeet/watni/base/CommonResult.kt
@@ -0,0 +1,26 @@
+package com.depromeet.watni.base
+
+/*
+ * Created by yunji on 13/04/2020
+ */
+data class CommonResult<ITEM>(
+    val status: CommonStatus,
+    val item: ITEM?,
+    val msg: String?
+) {
+
+    companion object {
+        fun <T> success(data: T?): CommonResult<T> =
+            CommonResult(CommonStatus.SUCCESS, data, CommonStatus.SUCCESS.toString())
+
+        fun <T> error(msg: String, data: T? = null): CommonResult<T> =
+            CommonResult(CommonStatus.ERROR, data, msg)
+
+        fun <T> loading(data: T? = null): CommonResult<T> =
+            CommonResult(CommonStatus.LOADING, data, CommonStatus.LOADING.toString())
+    }
+}
+
+enum class CommonStatus {
+    SUCCESS, ERROR, LOADING
+}

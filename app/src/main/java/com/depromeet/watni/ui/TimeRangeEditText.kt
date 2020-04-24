af1db83026c1eedc0184edf757a6a40edb980c82
zion830 <zion830@honglab.org>
2020-04-25 01:36:31 +0900

    feat: 커스텀뷰에 Timestamp 반환함수 추가 (#12)

--- app/src/main/java/com/depromeet/watni/ui/TimeRangeEditText.kt
+++ app/src/main/java/com/depromeet/watni/ui/TimeRangeEditText.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.annotation.IntRange
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.math.MathUtils
 import com.depromeet.watni.util.DateTimeUtil
 
 
@@ -15,6 +16,26 @@ class TimeRangeEditText @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : AppCompatEditText(context, attrs, defStyleAttr) {
+    var startHour = 0
+        set(value) {
+            field = MathUtils.clamp(value, 1, 12)
+            setText(DateTimeUtil.getReadableTimeString(startHour, startMin, endHour, endMin))
+        }
+    var startMin = 0
+        set(value) {
+            field = MathUtils.clamp(value, 1, 60)
+            setText(DateTimeUtil.getReadableTimeString(startHour, startMin, endHour, endMin))
+        }
+    var endHour = 0
+        set(value) {
+            field = MathUtils.clamp(value, 1, 12)
+            setText(DateTimeUtil.getReadableTimeString(startHour, startMin, endHour, endMin))
+        }
+    var endMin = 0
+        set(value) {
+            field = MathUtils.clamp(value, 1, 60)
+            setText(DateTimeUtil.getReadableTimeString(startHour, startMin, endHour, endMin))
+        }
 
     init {
         isClickable = true
@@ -27,6 +48,13 @@ class TimeRangeEditText @JvmOverloads constructor(
         @IntRange(from = DateTimeUtil.HOUR_MIN, to = DateTimeUtil.HOUR_MAX) endHour: Int,
         @IntRange(from = DateTimeUtil.MINUTES_MIN, to = DateTimeUtil.MINUTES_MAX - 1) endMin: Int
     ) {
-        setText(DateTimeUtil.getReadableTimeString(startHour, startMin, endHour, endMin))
+        this.startHour = startHour
+        this.startMin = startMin
+        this.endHour = endHour
+        this.endMin = endMin
     }
+
+    fun getStartTimestamp() = 1000 * ((startHour * 60) + startMin)
+
+    fun getEndTimestamp() = 1000 * ((endHour * 60) + endHour)
 }
